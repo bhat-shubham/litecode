f46a533574a6a4539c3f2554ffa1af0410a5d18b
Om Rindhe <rindheom43.com>
2024-10-09 23:55:29 +0530

    added sponsors

---
+++ litecode/script.js
@@ -0,0 +1,31 @@
+
+
+const faqBoxes = document.querySelectorAll('.faq-box');
+
+faqBoxes.forEach(faqBox => {
+  faqBox.addEventListener('click', () => {
+    // Toggle the active class on the current faq-box
+    faqBox.classList.toggle('active');
+
+    // Remove the active class from all other faq-boxes
+    faqBoxes.forEach(otherFaqBox => {
+      if (otherFaqBox !== faqBox) {
+        otherFaqBox.classList.remove('active');
+      }
+    });
+
+    // Scroll the previous active faq-box up
+    const previouslyActiveFaqBox = Array.from(faqBoxes).find(faq => faq.classList.contains('active') && faq !== faqBox);
+    if (previouslyActiveFaqBox) {
+      previouslyActiveFaqBox.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+var loaderElement = document.getElementById("loader");
+
+// function hideLoader() {
+//   loaderElement.style.display = "none";
+// }
+setTimeout(function()  {
+  loaderElement.style.display="none";
+},5000);
